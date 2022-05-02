ecb32ed6672ff750da503bf6eeb41be54b8957f3
ggernigon <guillaume.gernigon@outlook.fr>
2022-05-02 10:07:59 +0200

    finish first algo

---
+++ scripts/pages/index.js
@@ -0,0 +1,95 @@
+function displayRecipe(id, name, time, description, ingredients) {
+  recipesContainer.innerHTML += `<div class="col-4">
+  <article class="card">
+    <div class="card-header"></div>
+    <div class="card-body container grey-bg">
+      <div class="row">
+        <div class="col-9">
+          <h2 class="fs-6">${name}</h2>
+        </div>
+        <div class="col-3">
+          <p class="fs-6 fw-bolder">
+            <i class="fa-solid fa-clock"></i> ${time} min
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <ul class="list-unstyled ingredient-list-${id}">
+          </ul>
+        </div>
+        <div class="col">
+          <p>
+            ${description}
+          </p>
+        </div>
+      </div>
+    </div>
+  </article>
+</div>`;
+  const ingredientContainer = document.querySelector(`.ingredient-list-${id}`);
+  ingredientContainer.innerHTML = "";
+  ingredients.forEach((i) => {
+    if (i.quantity && i.unit) {
+      ingredientContainer.innerHTML += `<li class="fw-bolder">
+        ${i.ingredient}:
+        <span class="fw-normal">${i.quantity} ${i.unit}</span>
+      </li>`;
+    } else if(i.quantity && !i.unit) {
+      ingredientContainer.innerHTML += `<li class="fw-bolder">
+        ${i.ingredient}:
+        <span class="fw-normal">${i.quantity}</span>
+      </li>`;
+    } else {
+      ingredientContainer.innerHTML += `<li class="fw-bolder">
+        ${i.ingredient}
+      </li>`;
+    }
+  });
+}
+
+function displayAllRecipes(){
+  recipes.forEach((r) => {
+    displayRecipe(r.id, r.name, r.time, r.description, r.ingredients);
+  });
+}
+
+function search(word) {
+  recipesContainer.innerHTML = "";
+  for (let i = 0; i < recipes.length; i++) {
+    if (
+      recipes[i].name.toLowerCase().includes(word.toLowerCase()) ||
+      recipes[i].description.toLowerCase().includes(word.toLowerCase())
+    ) {
+      displayRecipe(recipes[i].id, recipes[i].name, recipes[i].time, recipes[i].description, recipes[i].ingredients);
+    } else {
+      for (let j = 0; j < recipes[i].ingredients.length; j++) {
+        if (
+          recipes[i].ingredients[j].ingredient
+            .toLowerCase()
+            .includes(word.toLowerCase())
+        ) {
+          displayRecipe(
+            recipes[i].id,
+            recipes[i].name,
+            recipes[i].time,
+            recipes[i].description,
+            recipes[i].ingredients
+          );
+          j = recipes[i].ingredients.length;
+        }
+      }
+    }
+  }
+}
+
+const recipesContainer = document.getElementById("recipes-container");
+displayAllRecipes();
+const inputSearch = document.querySelector(".input-search");
+inputSearch.addEventListener("keyup", () => {
+  if (inputSearch.value.length >= 3) {
+    search(inputSearch.value);
+  } else {
+    displayAllRecipes()
+  }
+});
